450c5d63bff45355be805fef09ef7d063404c3c7
nelson lassa <129919174+NelsonLassa@users.noreply.github.com>
2023-09-21 19:12:14 -0300

    Clase15 variable y operaciones 1

--- clase15/Javascript- Variable y operaciones 1/js/script.js
+++ clase15/Javascript- Variable y operaciones 1/js/script.js
@@ -1,4 +1,4 @@
-/* calcilo de volumen de paquetes para enviar
+//calculo de volumen de paquetes para enviar
 
 let anchoPaquete = prompt("Ancho: ");
 let altoPaquete= prompt("Alto: ");
@@ -8,11 +8,11 @@ const profundidadPaquete = 10;
 let volumen = anchoPaquete * altoPaquete * profundidadPaquete;
 alert(" El volumen del paquete es" + volumen);
 
-Calculo moneda */
+//Calculo moneda 
 
 let cantidadDolares = prompt("Cuantos dolares tenes: ");
 let tipoCambio = 733;
 
 const cambioMonedaLocal = cantidadDolares*tipoCambio;
 
-alert("Tenees $"+cambioMonedaLocal+"en dolares");
+alert("Tenees $"+cambioMonedaLocal+"en pesos");
