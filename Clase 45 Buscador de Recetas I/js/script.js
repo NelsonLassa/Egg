b1809023a824cdfb189a5b591c18b03d97ed3c72
Lassa <nelsonlassa63@gmail.com>
2024-01-18 23:18:05 -0300

    API de Buscador de recetas

--- Clase 45 Buscador de Recetas I/js/script.js
+++ Clase 45 Buscador de Recetas I/js/script.js
@@ -1,43 +1,53 @@
-const botonParaBuscar = document.getElementById("miBoton");
-const contenedorDeRceta = document.getElementById("container");
+/**
+ * Se obtienen referencias del Boton y
+ * donde se guardan las recetas obtenidas
+ */
 
-botonParaBuscar.addEventListener("click",  ()=>{
-    mostrarReceta()
-})
-async function mostrarReceta(){
-    let recetas = await obtenerReceta();
-    const recetaABuscar = document.getElementById("miInput").value;
-    console.log(recetas);
-    console.log(recetaABuscar);
-    recetas.forEach(receta => {
-        for (let i = 0; i < 20; i++) {
-            const ingrediente = receta[`strInfredient${i}`];
-            if (ingrediente == recetaABuscar) {
-                console.log("AQUI");
-                let contenedor = document.createElement("div");
-                let name = document.createElement("h1");
-                name.textContent = receta.strMeal;
-                let imagen = document.createElement("img");
-                imagen.src = document.strMealThumb; 
-                let massage = document.createElement("p");
-                massage.textContent = receta.strInstructions;
-                const ingredientesReceta = document.createElement('ul');
-                for (let i = 1; i <= 20; i++) {
-                    const ingrediente = receta[`strIngredient${i}`];
-                    if (ingrediente) {
-                        const li = document.createElement('li');
-                        li.textContent = `${ingrediente} - ${receta[`strMeasure${i}`]}`;
-                        ingredientesReceta.appendChild(li);
-                    }
-                }
-                contenedor.appendChild(name);
-                contenedor.appendChild(imagen);
-                contenedor.appendChild(message);
-                contenedor.appendChild(ingredientesReceta);
-                contenedorDeReceta.appendChild(contenedor);            }
-            
+const botonParaBuscar = document.getElementById("miBoton");
+/**
+ * Evento de clic en boton
+ */
+botonParaBuscar.addEventListener("click", () => {
+  mostrarReceta();
+});
+
+async function mostrarReceta() {
+  const muestraReceta = document.getElementById("container");
+
+  let recetas = await obtenerReceta();
+  const recetaABuscar = document.getElementById("miInput").value;
+  console.log(recetas);
+  console.log(recetaABuscar);
+  recetas.forEach(receta => {
+    for (let i = 1; i <= 20; i++) {
+      const ingrediente = receta[`strIngredient${i}`];
+      if (ingrediente && ingrediente.toLowerCase() === recetaABuscar) {
+        console.log("AQUI");
+        let contenedor = document.createElement("div");
+        contenedor.id = "recetaContainer";
+        let nombre = document.createElement("h1");
+        nombre.textContent = receta.strMeal;
+        let imagen = document.createElement("img");
+        imagen.src = receta.strMealThumb;
+        let mensaje = document.createElement("p");
+        mensaje.textContent = receta.strInstructions;
+        const ingredientesDeRecetas = document.createElement("ul");
+        for (let i = 1; i <= 20; i++) {
+          const ingrediente = receta[`strIngredient${i}`];
+          if (ingrediente) {
+            const liElement = document.createElement("li");
+            liElement.textContent = `${ingrediente} - ${receta[`strMeasure${i}`]}`;
+            ingredientesDeRecetas.appendChild(liElement);
+          }
         }
-    });
+        contenedor.appendChild(nombre);
+        contenedor.appendChild(imagen);
+        contenedor.appendChild(mensaje);
+        contenedor.appendChild(ingredientesDeRecetas);
+        muestraReceta.appendChild(contenedor);
+      }
+    }
+  });
 }
 
 async function obtenerReceta() {
